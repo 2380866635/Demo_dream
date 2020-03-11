d522564f4ad7fc874db10658a0baec891def7075
2380866635 <2380866635@qq.com>
2020-03-11 19:22:42 +0800

    开始创建好的环境和架构  外加一些刚刚开始的前端文件 这里的manager运行需要zookeeper

--- dream_manager_web/src/main/java/com/dream/controller/ItemController.java
+++ dream_manager_web/src/main/java/com/dream/controller/ItemController.java
@@ -1,11 +1,13 @@
 package com.dream.controller;
 
 
+import com.dream.common.pojo.EasyUiDataGridResult;
 import com.dream.pojo.TbItem;
 import com.dream.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +20,10 @@ public class ItemController {
     public TbItem selectByKey(@PathVariable long itemId){
         return itemService.selectByKey(itemId);
     }
+    @RequestMapping("/list")
+    public EasyUiDataGridResult list(@RequestParam("page")int pageNum,
+                                     @RequestParam("rows")int pageSize){
+        return itemService.list(pageNum,pageSize);
+    }
+
 }
