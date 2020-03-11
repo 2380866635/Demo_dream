0e6f3139349a2e92043013d608428c921c05494c
2380866635 <2380866635@qq.com>
2020-03-11 18:15:28 +0800

    开始创建好的环境和架构  外加一些刚刚开始的前端文件 这里的manager运行需要zookeeper

--- dream_common/src/main/java/com/dream/common/pojo/EasyUiDataGridResult.java
+++ dream_common/src/main/java/com/dream/common/pojo/EasyUiDataGridResult.java
@@ -1,6 +1,33 @@
 package com.dream.common.pojo;
 
-public class EasyUiDataGridResult {
+import java.io.Serializable;
+import java.util.List;
 
+public class EasyUiDataGridResult implements Serializable {
+    private Long total;
+    private List<?> rows;//?Ïàµ±ÓÚObject
 
+    public EasyUiDataGridResult() {
+    }
+
+    public EasyUiDataGridResult(Long total, List<?> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public List<?> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<?> rows) {
+        this.rows = rows;
+    }
 }
